9953bb8c6492aa29a105351e8219207cd9b347d0
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2025-04-19 16:22:08 -0400

    Tweaks
    * Added indexes
    * Added sessions table
    * Added Game Features/Art tables
    * Added Developer Art table

--- pkg/types/accounts.go
+++ pkg/types/accounts.go
@@ -8,12 +8,24 @@ import (
 )
 
 type User struct {
-	ID        string             `gorm:"primaryKey;type:char(26);unique;not null"`
-	Username  string             `gorm:"unique;not null;type:varchar(30)"`
-	Email     string             `gorm:"unique;not null;type:varchar(255)"`
-	Password  string             `gorm:"type:varchar(255)"`
-	State     bitfield.Bitfield8 `gorm:"not null;default:0"`
+	ID       string `gorm:"primaryKey;type:char(26);unique;not null"`
+	Username string `gorm:"unique;not null;type:varchar(30)"`
+	Email    string `gorm:"unique;not null;type:varchar(255)"`
+
+	// A scrypted hash of the user's password
+	Password string `gorm:"type:varchar(255)"`
+
+	// A base64 encoded secret based on a randomly generated 256-bit key
+	// that is encrypted using the server's secret key.
+	Secret string `gorm:"type:char(32)"`
+
+	// A 8-bit field of flags that are used to determine the properties of the user.
+	State bitfield.Bitfield8 `gorm:"not null;default:0"`
+
+	// The date and time when the user's account was created
 	CreatedAt time.Time
+
+	// The date and time when the user's account was last updated
 	UpdatedAt time.Time
 
 	Google       *UserGoogle
@@ -24,10 +36,24 @@ type User struct {
 	Recovery     []*RecoveryCode
 	Developer    *DeveloperMember
 	UserGameSave []*UserGameSave
+	UserSession  []*UserSession
+}
+
+type UserSession struct {
+	ID        string `gorm:"primaryKey;type:varchar(26);unique;not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null;index:idx_user_session_user_id"`
+	UserAgent string `gorm:"type:varchar(255);not null"`
+	Origin    string `gorm:"type:varchar(255);not null"`
+	IP        string `gorm:"type:varchar(255);not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	ExpiresAt time.Time
+
+	User User `gorm:"constraint:OnDelete:CASCADE;"`
 }
 
 type UserGoogle struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null;index:idx_user_google_user_id"`
 	GoogleID  string `gorm:"primaryKey;type:varchar(255);unique;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -36,7 +62,7 @@ type UserGoogle struct {
 }
 
 type UserDiscord struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null;index:idx_user_discord_user_id"`
 	DiscordID string `gorm:"primaryKey;type:varchar(255);unique;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -45,7 +71,7 @@ type UserDiscord struct {
 }
 
 type UserGitHub struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null;index:idx_user_git_hub_user_id"`
 	GitHubID  string `gorm:"primaryKey;type:varchar(255);unique;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -54,7 +80,7 @@ type UserGitHub struct {
 }
 
 type UserTOTP struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);unique;not null;index:idx_user_totp_user_id"`
 	Secret    string `gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -63,7 +89,7 @@ type UserTOTP struct {
 }
 
 type Verification struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null;index:idx_verification_user_id"`
 	Code      string `gorm:"type:char(6);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -73,7 +99,7 @@ type Verification struct {
 }
 
 type RecoveryCode struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null"`
+	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null;index:idx_recovery_code_user_id"`
 	Code      string `gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
--- pkg/types/developers.go
+++ pkg/types/developers.go
@@ -7,31 +7,36 @@ import (
 )
 
 type Developer struct {
-	ID        string             `gorm:"primaryKey;type:char(26);unique;not null"`
-	Name      string             `gorm:"type:varchar(255);not null"`
-	State     bitfield.Bitfield8 `gorm:"not null;default:0"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-
-	Games   []*DeveloperGame
-	Members []*DeveloperMember
-}
-
-type DeveloperGame struct {
 	ID          string             `gorm:"primaryKey;type:char(26);unique;not null"`
-	DeveloperID string             `gorm:"foreignKey:DeveloperID;type:char(26);not null"`
-	Name        string             `gorm:"type:varchar(255);not null"`
+	Name        string             `gorm:"type:varchar(255);not null;default:''"`
+	Description string             `gorm:"type:varchar(255);not null;default:''"`
 	State       bitfield.Bitfield8 `gorm:"not null;default:0"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 
-	Developer Developer
-	GameSave  []*UserGameSave
+	Games   []*DeveloperGame
+	Members []*DeveloperMember
+	Art     []*DeveloperArt
+}
+
+type DeveloperGame struct {
+	ID          string             `gorm:"primaryKey;type:char(26);unique;not null"`
+	DeveloperID string             `gorm:"foreignKey:DeveloperID;type:char(26);not null;index:idx_developer_game_developer_id"`
+	Name        string             `gorm:"type:varchar(255);not null;default:'';index:idx_developer_game_name"`
+	Description string             `gorm:"type:varchar(255);not null;default:''"`
+	State       bitfield.Bitfield8 `gorm:"not null;default:0"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+
+	Developer   Developer `gorm:"constraint:OnDelete:CASCADE;"`
+	GameSave    []*UserGameSave
+	GameFeature []*GameFeature
+	GameArt     []*GameArt
 }
 
 type DeveloperMember struct {
-	UserID      User                `gorm:"foreignKey:UserID;type:char(26);not null"`
-	DeveloperID string              `gorm:"foreignKey:DeveloperID;type:char(26);not null"`
+	UserID      User                `gorm:"foreignKey:UserID;type:char(26);not null;index:idx_developer_member_user_id"`
+	DeveloperID string              `gorm:"foreignKey:DeveloperID;type:char(26);not null;index:idx_developer_member_developer_id"`
 	State       bitfield.Bitfield16 `gorm:"not null;default:0"`
 	Linked      time.Time           `gorm:"not null"`
 	CreatedAt   time.Time
@@ -40,3 +45,48 @@ type DeveloperMember struct {
 	Developer Developer `gorm:"constraint:OnDelete:CASCADE;"`
 	User      User      `gorm:"constraint:OnDelete:CASCADE;"`
 }
+
+type GameFeature struct {
+	ID              string `gorm:"primaryKey;type:char(26);unique;not null"`
+	DeveloperGameID string `gorm:"foreignKey:DeveloperGameID;type:char(26);not null;index:idx_game_feature_developer_game_id"`
+	TagID           string `gorm:"type:varchar(20);unique;not null;default:''"`
+	TagDescription  string `gorm:"type:varchar(255);not null;default:''"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+
+	DeveloperGame DeveloperGame `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+type GameArt struct {
+	ID              string `gorm:"primaryKey;type:char(26);unique;not null"`
+	DeveloperGameID string `gorm:"foreignKey:DeveloperGameID;type:char(26);not null;index:idx_game_art_developer_game_id"`
+	ArtType         string `gorm:"type:varchar(20);not null;default:''"`
+	ArtLink         string `gorm:"type:varchar(255);not null;default:''"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+
+	DeveloperGame DeveloperGame `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+type DeveloperArt struct {
+	ID          string `gorm:"primaryKey;type:char(26);unique;not null"`
+	DeveloperID string `gorm:"foreignKey:DeveloperID;type:char(26);not null;index:idx_developer_art_developer_id"`
+	ArtType     string `gorm:"type:varchar(20);not null;default:''"`
+	ArtLink     string `gorm:"type:varchar(255);not null;default:''"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+
+	Developer Developer `gorm:"constraint:OnDelete:CASCADE;"`
+}
+
+/* {
+	"ImageURL":   "",
+	"Title":      ". . .",
+	"Text":       "",
+	"Creator":    "",
+	"FooterText": "",
+	"Enabled":    false,
+	"IsNew":      false,
+	"ID":         "",
+	"Features":   []map[string]any{},
+} */

8b796aca7469a5dbbc4db14f868dc7536d018a41
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2025-05-22 10:40:49 -0400

    Update types.go

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -18,14 +18,15 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 
-	Avatar        *Image          `gorm:"foreignKey:AvatarID;constraint:OnDelete:SET NULL;"`
-	Banner        *Image          `gorm:"foreignKey:BannerID;constraint:OnDelete:SET NULL;"`
-	UserGameSaves []*UserGameSave `gorm:"foreignKey:UserID"`
+	Avatar          *Image             `gorm:"foreignKey:AvatarID;references:ID;constraint:OnDelete:SET NULL;"`
+	Banner          *Image             `gorm:"foreignKey:BannerID;references:ID;constraint:OnDelete:SET NULL;"`
+	UserGameSaves   []*UserGameSave    `gorm:"foreignKey:UserID"`
+	DeveloperMember []*DeveloperMember `gorm:"foreignKey:UserID"`
 }
 
 type UserSession struct {
 	ID        string `gorm:"primaryKey;type:char(26);unique;not null"`
-	UserID    string `gorm:"foreignKey:UserID;not null;"`
+	UserID    string `gorm:"not null"`
 	UserAgent string `gorm:"mediumtext;not null"`
 	Origin    string `gorm:"mediumtext;not null"`
 	IP        string `gorm:"mediumtext;not null"`
@@ -33,104 +34,104 @@ type UserSession struct {
 	UpdatedAt time.Time
 	ExpiresAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserGoogle struct {
 	ID        string `gorm:"primaryKey;type:varchar(255);not null;unique;"`
-	UserID    string `gorm:"foreignKey:UserID;not null;"`
+	UserID    string `gorm:"not null"`
 	CreatedAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserDiscord struct {
 	ID        string `gorm:"primaryKey;type:varchar(255);not null;unique;"`
-	UserID    string `gorm:"foreignKey:UserID;not null;"`
+	UserID    string `gorm:"not null"`
 	CreatedAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserGitHub struct {
 	ID        string `gorm:"primaryKey;type:varchar(255);not null;unique;"`
-	UserID    string `gorm:"foreignKey:UserID;not null;"`
+	UserID    string `gorm:"not null"`
 	CreatedAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserTOTP struct {
-	UserID    string `gorm:"foreignKey:UserID;not null;"`
+	UserID    string `gorm:"not null"`
 	Secret    string `gorm:"type:mediumtext;not null"`
 	CreatedAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type Verification struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null;"`
+	UserID    string `gorm:"type:char(26);not null"`
 	Code      string `gorm:"type:mediumtext;not null"`
 	CreatedAt time.Time
 	ExpiresAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type RecoveryCode struct {
-	UserID    string `gorm:"foreignKey:UserID;type:char(26);not null;"`
+	UserID    string `gorm:"type:char(26);not null"`
 	Code      string `gorm:"type:mediumtext;not null"`
 	CreatedAt time.Time
 
-	User *User `gorm:"constraint:OnDelete:CASCADE;"`
+	User *User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type Achievement struct {
 	ID              string `gorm:"primaryKey;type:char(26);unique;not null"`
-	UserID          string `gorm:"foreignKey:UserID;not null;"`
-	DeveloperGameID string `gorm:"foreignKey:DeveloperGameID;not null;"`
+	UserID          string `gorm:"not null"`
+	DeveloperGameID string `gorm:"not null"`
 	Description     string `gorm:"type:tinytext;not null"`
 	Points          uint64 `gorm:"not null;default:0"`
 	IconID          *string
 	CreatedAt       time.Time
 
-	User          *User          `gorm:"constraint:OnDelete:CASCADE;"`
-	DeveloperGame *DeveloperGame `gorm:"constraint:OnDelete:CASCADE;"`
-	Icon          *Image         `gorm:"foreignKey:IconID;constraint:OnDelete:SET NULL;"`
+	User          *User          `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
+	DeveloperGame *DeveloperGame `gorm:"foreignKey:DeveloperGameID;references:ID;constraint:OnDelete:CASCADE;"`
+	Icon          *Image         `gorm:"foreignKey:IconID;references:ID;constraint:OnDelete:SET NULL;"`
 }
 
 type SystemEvent struct {
 	ID         string `gorm:"primaryKey;type:char(26);unique;not null"`
-	EventID    string `gorm:"foreignKey:EventID;"`
+	EventID    string
 	Details    string `gorm:"type:tinytext"`
 	Successful bool
 	CreatedAt  time.Time
 
-	Event *Event `gorm:"constraint:OnDelete:CASCADE;"`
+	Event *Event `gorm:"foreignKey:EventID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserEvent struct {
 	ID         string `gorm:"primaryKey;type:char(26);unique;not null"`
-	UserID     string `gorm:"foreignKey:UserID;not null;"`
-	EventID    string `gorm:"foreignKey:EventID;"`
+	UserID     string `gorm:"not null"`
+	EventID    string
 	Details    string `gorm:"type:tinytext"`
 	Successful bool
 	CreatedAt  time.Time
 
-	User  *User  `gorm:"constraint:OnDelete:CASCADE;"`
-	Event *Event `gorm:"constraint:OnDelete:CASCADE;"`
+	User  *User  `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
+	Event *Event `gorm:"foreignKey:EventID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type DeveloperEvent struct {
 	ID          string `gorm:"primaryKey;type:char(26);unique;not null"`
-	DeveloperID string `gorm:"foreignKey:DeveloperID;"`
-	EventID     string `gorm:"foreignKey:EventID;"`
+	DeveloperID string
+	EventID     string
 	Details     string `gorm:"type:tinytext"`
 	Successful  bool
 	CreatedAt   time.Time
 
-	Developer *Developer `gorm:"constraint:OnDelete:CASCADE;"`
-	Event     *Event     `gorm:"constraint:OnDelete:CASCADE;"`
+	Developer *Developer `gorm:"foreignKey:DeveloperID;references:ID;constraint:OnDelete:CASCADE;"`
+	Event     *Event     `gorm:"foreignKey:EventID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type UserGameSave struct {
@@ -138,12 +139,11 @@ type UserGameSave struct {
 	DeveloperGameID string `gorm:"primaryKey;type:char(26);not null"`
 	SaveSlot        uint8  `gorm:"not null;min:1;max:10"`
 	SaveData        string `gorm:"type:mediumtext"`
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 
-	CreatedAt time.Time
-	UpdatedAt time.Time
-
-	User          *User          `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"`
-	DeveloperGame *DeveloperGame `gorm:"foreignKey:DeveloperGameID;constraint:OnDelete:CASCADE;"`
+	User          *User          `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
+	DeveloperGame *DeveloperGame `gorm:"foreignKey:DeveloperGameID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type Developer struct {
@@ -156,32 +156,33 @@ type Developer struct {
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 
-	Banner *Image `gorm:"foreignKey:BannerID;constraint:OnDelete:SET NULL;"`
-	Avatar *Image `gorm:"foreignKey:AvatarID;constraint:OnDelete:SET NULL;"`
+	Banner           *Image             `gorm:"foreignKey:BannerID;references:ID;constraint:OnDelete:SET NULL;"`
+	Avatar           *Image             `gorm:"foreignKey:AvatarID;references:ID;constraint:OnDelete:SET NULL;"`
+	DeveloperMembers []*DeveloperMember `gorm:"foreignKey:DeveloperID"`
 }
 
 type DeveloperGame struct {
-	ID          string             `gorm:"primaryKey;type:char(26);unique;not null"`
-	Name        string             `gorm:"type:tinytext;not null;default:''"`
-	Description string             `gorm:"type:mediumtext"`
-	DeveloperID string             `gorm:"foreignKey:DeveloperID;"`
+	ID          string `gorm:"primaryKey;type:char(26);unique;not null"`
+	Name        string `gorm:"type:tinytext;not null;default:''"`
+	Description string `gorm:"type:mediumtext"`
+	DeveloperID string
 	State       bitfield.Bitfield8 `gorm:"not null;default:0;"`
 	ThumbnailID *string
 	CreatedAt   time.Time
 
-	Thumbnail     *Image          `gorm:"foreignKey:ThumbnailID;constraint:OnDelete:SET NULL;"`
-	Developer     *Developer      `gorm:"constraint:OnDelete:CASCADE;"`
+	Thumbnail     *Image          `gorm:"foreignKey:ThumbnailID;references:ID;constraint:OnDelete:SET NULL;"`
+	Developer     *Developer      `gorm:"foreignKey:DeveloperID;references:ID;constraint:OnDelete:CASCADE;"`
 	Features      []*FeatureTag   `gorm:"many2many:developer_game_features;"`
 	UserGameSaves []*UserGameSave `gorm:"foreignKey:DeveloperGameID"`
 }
 
 type DeveloperMember struct {
-	UserID      string             `gorm:"foreignKey:UserID;"`
-	DeveloperID string             `gorm:"foreignKey:DeveloperID;"`
+	UserID      string             `gorm:"not null"`
+	DeveloperID string             `gorm:"not null"`
 	State       bitfield.Bitfield8 `gorm:"not null;default:0;"`
 
-	User      *User      `gorm:"constraint:OnDelete:CASCADE;"`
-	Developer *Developer `gorm:"constraint:OnDelete:CASCADE;"`
+	User      *User      `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"`
+	Developer *Developer `gorm:"foreignKey:DeveloperID;references:ID;constraint:OnDelete:CASCADE;"`
 }
 
 type Image struct {
